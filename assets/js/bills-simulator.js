fd5b168b113c0f1440b308661e5f0c4881aa084b
DANILA <greppidanila@gmail.com>
2022-11-12 17:33:48 -0300

    add sweet alert

--- assets/js/bills-simulator.js
+++ assets/js/bills-simulator.js
@@ -1,63 +1,65 @@
 //Función Calcular Gastos
 function simular_costo() {
     
-    // variable macroeconómica
-    var tipo_cambio_dolar = 160
+//Variable macroeconómica
+    let tipo_cambio_dolar = 160   
     //dolar oficial
     
-    // variable de operación elegido por cliente
-    var precio_transaccion = document.getElementById("precio_transaccion").value;
+//Variable de operación elegido por cliente
+    let precio_transaccion = document.getElementById("precio_transaccion").value; 
     //a continuación, doy ejemplo si fueran 100000usd
-    var precio_escritura = precio_transaccion;
+    let precio_escritura = precio_transaccion; 
     //100000usd
 
-    var comision_comprador = document.getElementById("comision_comprador").value / 100
-    // 0.04 (caso de ejemplo si se eligiera la totalidad de honorarios = 4%)
-    
-    // comision final con iva o no
-    var iva_comprador = document.getElementById("iva_comprador");
-    
-    if (iva_comprador.checked == true) {
-        comision_comprador *= 1.21
-        //0.04*1.21= 0.484 (4.84%)
-    } else {
-        comision_comprador = comision_comprador
-        //0.04 (4%)
-    } 
+    //si no se coloca un valor coherente como precio de transacción:
+    do{
+        Swal.fire({
+            title: '¿Existe ese valor de propiedad..?',
+            text: 'Ingrese un número correcto',
+            icon: 'error',
+            showConfirmButton: false,
+            timer: 2000
+        })
+    }while(precio_transaccion > 25000) 
+        
 
-// impuesto de sellos
-    var primervivienda_comprador = document.getElementById("primervivienda_comprador");
-    var exencion_sellos = 6250000 / tipo_cambio_dolar
+    let comision_comprador = document.getElementById("comision_comprador").value / 100
+    //0.04 (caso de ejemplo si se eligiera la totalidad de honorarios = 4%)
+    
+//Comision final con iva o no
+    let iva_comprador = document.getElementById("iva_comprador") 
+    
+    iva_comprador = iva_comprador.checked ? comision_comprador *= 1.21 : comision_comprador
+
+//Impuesto de sellos
+    let primervivienda_comprador = document.getElementById("primervivienda_comprador");
+    let exencion_sellos = 6250000 / tipo_cambio_dolar;
     // 6250000/160= 39062 usd (la exencion de sellos actualmente es desde los 6.250.000 pesos argentinos)
 
-    if (primervivienda_comprador.checked == true) {
-        precio_escritura -= parseInt(exencion_sellos) 
-        // precio_escritura = 100000usd - 39062 usd = son 60938 usd
-    } else {
-        sellos = 0.018 
-        //1,8% mitad de totalidad que es 3,6%, siguen siendo 100000usd
-    } 
+    let sellos = 0.018;
+    //1,8% mitad de totalidad que es 3,6%
 
+    primervivienda_comprador = primervivienda_comprador.checked ? precio_escritura -= parseInt(exencion_sellos) : sellos
 
-//  gastos de escrituración y honorarios de escribanía
-    gastos_escrituracion = 0.012   
+//Gastos de escrituración y honorarios de escribanía
+    let gastos_escrituracion = 0.012   
     //1,2% es el promedio de gastos
 
 
-// Generación de nuevas variables
-    monto_sellos = sellos * precio_escritura
+//Generación de nuevas letiables
+    let monto_sellos = sellos * precio_escritura
     //obtener el 1,8% de 60938 usd 
-    monto_gastos_escrituracion = gastos_escrituracion * precio_transaccion
+    let monto_gastos_escrituracion = gastos_escrituracion * precio_transaccion
     //1.2% * 100000usd 
-    monto_comision_comprador = comision_comprador * precio_transaccion
+    let monto_comision_comprador = comision_comprador * precio_transaccion
     //4.84% * 100000usd 
-    total_costo_comprador = monto_sellos + monto_gastos_escrituracion + monto_comision_comprador
+    let total_costo_comprador = monto_sellos + monto_gastos_escrituracion + monto_comision_comprador
     //suma de gastos 
-    plata_final_comprador = parseInt(precio_transaccion) + parseInt(total_costo_comprador)
+    let plata_final_comprador = parseInt(precio_transaccion) + parseInt(total_costo_comprador)
     //suma total gastos + precio de transaccion
 
 
-// Redondeo de números y sin decimales
+//Redondeo de números y sin decimales
     total_costo_comprador = Math.round(total_costo_comprador * 100) / 100;
     total_costo_comprador = total_costo_comprador.toFixed(0);
 
@@ -75,7 +77,7 @@ function simular_costo() {
 
 
 
-    //Resultados
+//Resultados
     document.getElementById("analisis_oferta").style.display = "block";
     document.getElementById("analisis_imagen").style.display = "block";
     
@@ -86,7 +88,7 @@ function simular_costo() {
     document.getElementById("gastos_escrituracion").innerHTML = monto_gastos_escrituracion;
     document.getElementById("monto_comision_comprador").innerHTML = monto_comision_comprador;
     
-    //Cambiar texto en btn
+//Cambiar texto en btn
     document.getElementById("calcular_oferta").innerHTML = "Recalcular";
     
 }
